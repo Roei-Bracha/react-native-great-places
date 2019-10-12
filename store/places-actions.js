e972a8ee45d80b65512b61d8b1d0eb3d1df8448b
Roei Bracha <roei635@gmail.com>
2019-10-12 11:36:58 +0300

    move the image to persistent storage

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,21 @@
 export const ADD_PLACE = 'ADD_PLACE'
+import * as FileSystem from 'expo-file-system';
+
 
 export const addPlace = (title, image) => {
-    return {type:ADD_PLACE,placeData:{title,image}}
+    return async (dispatch) => {
+        const fileName = image.split('/').pop() // look at the image path split it by / and take the last one - the temp image name
+        const newPath = FileSystem.documentDirectory + fileName
+        try {
+            await FileSystem.moveAsync({
+                from: image,
+                to: newPath
+            });
+        }
+        catch (err) {
+            console.log(err)
+            throw new Error(err)
+        }
+        dispatch({ type: ADD_PLACE, placeData: { title, image:newPath } })
+    }
 }
