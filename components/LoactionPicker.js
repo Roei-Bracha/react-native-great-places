80d6c2e66c1e42c13792160569a2d6165fbb7ad1
Roei Bracha <roei635@gmail.com>
2019-10-12 12:42:26 +0300

    getting the user location

---
+++ components/LoactionPicker.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import { View, Text, Button, ActivityIndicator, StyleSheet, Alert } from 'react-native';
+import Colors from '../constants/Colors';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+const LocationPicker = (props) => {
+    const [pickedLocation, setPickedLocation] = useState(null)
+    const [isFetching,setIsFetching] = useState(false)
+    const verifyPermission = async () => {
+        const result = await Permissions.askAsync(Permissions.LOCATION)
+        if (result.status !== 'granted') {
+            Alert.alert('Permissions Error', 'you need to allow us to use the location', [{ text: 'OK' }])
+            return false
+        }
+        return true
+    }
+
+    const getLocationHandler = async () => {
+        setIsFetching(true)
+        const hasPermission = await verifyPermission()
+        if (!hasPermission) { return }
+        try {
+            const location = await Location.getCurrentPositionAsync({ timeout: 5000 })
+            setPickedLocation({lat:location.coords.latitude, lng:location.coords.longitude})
+        }
+        catch(err){
+            Alert.alert('failed toLocate','failed to get your location try again later or chose a place from the map',[{text:'OK'}])
+        }
+        setIsFetching(false)
+    }
+    return (
+        <View style={styles.locationPicker}>
+            <View style={styles.mapPreview}>
+                {isFetching ? <ActivityIndicator size={'large'} color={Colors.primary}/> : <Text> no location chosen yet</Text>}
+            </View>
+            <Button title={'get user location'} color={Colors.primary} onPress={getLocationHandler}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    locationPicker: {
+        marginBottom: 15,
+    },
+    mapPreview: {
+        marginBottom: 10,
+        width: '100%',
+        height: 150,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+})
+
+export default LocationPicker
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,6 +4,7 @@ import Colors from '../constants/Colors';
 import { useDispatch } from 'react-redux'
 import { addPlace } from '../store/places-actions';
 import ImagePicker from '../components/ImagePicker';
+import LocationPicker from '../components/LoactionPicker';
 
 
 const NewPlaceScreen = (props) => {
@@ -30,8 +31,9 @@ const NewPlaceScreen = (props) => {
             <View style={styles.form}>
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.textInput} value={titleValue} onChangeText={titleChangeHandler} />
-                <ImagePicker onImageTaken={imageTakenHandler}/>
-                <Button title={"Save Place"} color={Colors.primary} onPress={savePlaceHandler}/>
+                <ImagePicker onImageTaken={imageTakenHandler} />
+                <LocationPicker/>
+                <Button title={"Save Place"} color={Colors.primary} onPress={savePlaceHandler} />
             </View>
         </ScrollView>
     )
